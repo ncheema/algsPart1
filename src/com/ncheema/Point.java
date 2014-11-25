34feb610ea39b026eea68d3af23a3b1a28610d24
ncheema <ncheema@ucsd.edu>
2014-11-25 02:45:54 -0800

    intital commit assignment 3

---
+++ src/com/ncheema/Point.java
@@ -0,0 +1,49 @@
+package com.ncheema;
+
+/**
+ * Created by navjotcheema on 11/25/14.
+ */
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER;       // YOUR DEFINITION HERE
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        /* YOUR CODE HERE */
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        if (this.y == that.y)
+            return this.x - that.y;
+        return this.y - that.y;
+    }
+
+    // return string representation of this point
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
