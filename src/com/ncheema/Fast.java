0903cf4e7a2e7e9337ba1a472de5fe3aef6ed078
ncheema <ncheema@ucsd.edu>
2014-12-01 02:21:08 -0800

    fixed bugs in drawing and searching for line segment

--- src/com/ncheema/Fast.java
+++ src/com/ncheema/Fast.java
@@ -2,6 +2,7 @@ package com.ncheema;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -24,29 +25,31 @@ public class Fast {
         //natural order sorting; Implemented comparable in Point.java
         Arrays.sort(points);
 
+        //draw & print the line segment;
         for (int p = 0; p < points.length-1; p++) {
-            Arrays.sort(points,p+1,points.length, points[p].SLOPE_ORDER);
+            Arrays.sort(points, points[p].SLOPE_ORDER);
             List<Point> listP = new ArrayList<Point>();
-            listP.add(points[p]);
+            listP.add(points[p]);   //starting point of line segment
             double slope = points[p].slopeTo(points[p+1]);
 
             for (int i = p+1; i< points.length; i++) {
                 if (slope == points[p].slopeTo(points[i])) {
                     listP.add(points[i]);
                 }
-                else if (listP.size() <3) {
+                else if (listP.size() <4) {
                     listP.clear();
                     slope = points[p].slopeTo(points[i]);
                 }
             }
             if (listP.size() > 3) {
+                Collections.sort(listP);
                 for (int j = 0; j < listP.size(); j++) {
                     StdOut.print(listP.get(j));
                     if (j + 1 != listP.size())
                         StdOut.print(" -> ");
                 }
                 StdOut.println();
-                points[p].drawTo(listP.get(listP.size()-1));
+                listP.get(0).drawTo(listP.get(listP.size() - 1));
                 listP.clear();
             }
 
