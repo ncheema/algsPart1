37b9a45150a97dcbe9967c10fe38017e4d67af27
ncheema <ncheema@ucsd.edu>
2015-01-09 17:06:51 -0800

    intial commit

--- src/com/ncheema/Solver.java
+++ src/com/ncheema/Solver.java
@@ -4,9 +4,33 @@ package com.ncheema;
  * Created by navjotcheema on 1/5/15.
  */
 public class Solver {
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
-    public boolean isSolvable()            // is the initial board solvable?
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
+    /**
+     * find a solution to the initial board (using the A* algorithm)
+     * @param initial
+     */
+    public Solver(Board initial)   {
+
+    }
+
+    /**
+     * is the initial board solvable?
+     * @return true if board is solvable, false otherwise
+     */
+    public boolean isSolvable(){
+
+    }
+
+    /**
+     *
+     * @return min number of moves to solve initial board; -1 if unsolvable
+     */
+    public int moves() {
+        if (isSolvable()) {
+
+        }
+        return -1;
+    }
     public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
     public static void main(String[] args) // solve a slider puzzle (given below)
+
 }
