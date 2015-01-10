7e7cc3e400c2a11a5e3c72503f2fc5d92f78a958
ncheema <ncheema@ucsd.edu>
2015-01-09 22:16:18 -0800

    intial commit

--- src/com/ncheema/Solver.java
+++ src/com/ncheema/Solver.java
@@ -1,14 +1,45 @@
 package com.ncheema;
 
+import java.util.Comparator;
+
 /**
  * Created by navjotcheema on 1/5/15.
  */
 public class Solver {
+    MinPQ<Board> minPQ;
+    Comparator<Node> boardComparator = new BoardComparator();
+
+    private class BoardComparator implements Comparator<Node> {
+        
+    }
+
+    private class Node {
+        Board board;
+        Node parent;
+
+        //constructor for root
+        public Node(Board b) {
+            this.board = b;
+            this.parent = null; //root node
+        }
+        //constructor for child nodes
+        public Node(Board b, Node parent) {
+            this.board = b;
+            this.parent = parent;
+        }
+
+
+    }
+
     /**
      * find a solution to the initial board (using the A* algorithm)
      * @param initial
      */
     public Solver(Board initial)   {
+        minPQ = new MinPQ<Board>();
+        minPQ.insert(initial);  //add the initial board
+
+        minPQ.insert(initial.neighbors());
 
     }
 
@@ -31,6 +62,6 @@ public class Solver {
         return -1;
     }
     public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
-    public static void main(String[] args) // solve a slider puzzle (given below)
+    public static void main(String[] args) {} // solve a slider puzzle (given below)
 
 }
